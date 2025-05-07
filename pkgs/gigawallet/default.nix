23f56dd7ecddc4ce74ab8c295bc6eb9453eb5d83
Adam Brady <adam@boxxen.org>
2025-05-07 21:14:34 +1000

    Gigawallet v1.0.6

--- pkgs/gigawallet/default.nix
+++ pkgs/gigawallet/default.nix
@@ -8,11 +8,11 @@
 
 buildGoModule {
   pname = "gigawallet";
-  version = "1.0.5";
+  version = "1.0.6";
 
   src = fetchGit {
     url = "https://github.com/dogecoinfoundation/gigawallet.git";
-    ref = "refs/tags/v1.0.5";
+    ref = "refs/tags/v1.0.6";
   };
 
   vendorHash = "sha256-mW5SStSabjWIlLWarI0OfyCTRWRQnEbk2BXabJCJ2h4";
