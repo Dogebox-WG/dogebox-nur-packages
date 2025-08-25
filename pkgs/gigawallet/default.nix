5c570f4574f7ad7e596c7d7d0c2289c6a6141b76
Adam Brady <adam@boxxen.org>
2025-08-25 14:20:05 +1000

    gigawallet: go_122 is EOL in nixos 25.05

--- pkgs/gigawallet/default.nix
+++ pkgs/gigawallet/default.nix
@@ -18,7 +18,7 @@ buildGoModule rec {
   vendorHash = "sha256-mW5SStSabjWIlLWarI0OfyCTRWRQnEbk2BXabJCJ2h4";
 
   nativeBuildInputs = [
-    pkgs.go_1_22
+    pkgs.go_1_23
     pkgs.pkg-config
   ];
 
